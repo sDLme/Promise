6c2e13e8b8e28d80dd39e68435f90c164592021c
Marina_Sharun <marina2606@gmail.com>
2023-01-27 19:27:15 +0200

    reworked code with the best practises

--- promise.js
+++ promise.js
@@ -1,50 +1,37 @@
-
 function solution(N) {
-  const likedArr = [
-    {
-      id: 14,
-      name: 'LikeBrands1'
-    },
-    {
-      id: 14,
-      name: 'LikeBrands'
-    },
-    
-  ]
+  
+  // likedArr and brandArr are arrays of objects representing brands
+  const likedArr = [{id: 14, name: 'LikeBrands1'}, {id: 14, name: 'LikeBrands'}];
+  const brandArr = [{id: 14, name: 'LikeBrands1'}, {id: 8, name: 'Brand2'}, {id: 6, name: 'Brand3'}];
 
-  const brandArr = [
-    {
-      id: 14,
-      name: 'LikeBrands1'
-    },
-    {
-      id: 8,
-      name: 'Brand2'
-    },
-    {
-      id: 6,
-      name: 'Brand3'
-    }
-  ]
+  // Assign resolved Promises of the arrays to variables pLiked and pBrand
+  const [pLiked, pBrand] = [Promise.resolve(likedArr), Promise.resolve(brandArr)];
 
-  pBrand = Promise.resolve(brandArr)
-  pLiked = Promise.resolve(likedArr)
-
-  return Promise.all([pLiked,pBrand]).then(data => {
+  // Use Promise.all to resolve both Promises and destructure the returned data
+  return Promise.all([pLiked,pBrand]).then(([liked, brand]) => {
     let result= [];
-    const concatArr = data[0].concat(data[1])
-                    ?.filter((value, index, self) => index === self.findIndex((t => t.name === value.name)));
+    // Combine the two arrays, and use reduce to filter out duplicate brand names
+    const concatArr = [...liked, ...brand].reduce((acc, { name }) => {
+      // If the brand name is not already in the accumulator, add it
+      if(acc.findIndex(({ name: n }) => n === name) === -1) {
+        acc.push({ name });
+      }
+      return acc;
+    }, []);
 
+    // If the filtered array is smaller than N, reject the promise
     if(concatArr.length < N) {
-      return Promise.reject('Something went wrong again')
+      return Promise.reject('Something went wrong')
     } else {
-      for(let i= 0; i < N; i++) {
-        result.push(concatArr[i].name)
+      // Else, add the first N names to the result array
+      for(let i = 0; i < N; i++) {
+        result.push(concatArr[i].name);
       }
       return result;
     }
   })
-  .catch(errorMessage => {
+  // If the promise is rejected, return the error message
+  .catch((errorMessage) => {
     return errorMessage
   })
 }
